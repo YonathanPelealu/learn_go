9ea8aef2eb468378ca21fc7a2a624d3fd5a443cd
andika.pelealu90 <andika.pelealu90@gmail.com>
2020-11-11 12:48:34 +0700

    initial commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+	// 	fmt.Println("SAtu = ", 1)
+	// 	fmt.Println("Dua = ", 2)
+	// 	fmt.Println("Tiga koma lima = ", 3.5)
+}
